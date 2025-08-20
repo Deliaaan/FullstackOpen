be84043d8e5c8e3cba257b2eb06b083037286ee8
dnava <delian.ngtmr@gmail.com>
2025-08-20 01:06:08 -0600

    part 2.9 done

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ]) 
-  const [newName, setNewName] = useState('Diego Nava')
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault() // esto evita que se recarge 
     console.log("click", event.target)
 
     const personObj = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
     
     console.log(persons)
@@ -21,40 +27,79 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObj))
       setNewName('')
+      setNewPhone('')
     }
-
-    // const verifyPersonExists = ((prevPersons) => {
-    //   if (prevPersons.some(p => p.name === persons.name)) {
-    //     alert(` ${personObj} ya existe`)
-    //   } else {
-    //     setPersons(persons.concat(personObj))
-    //   }
-    // })
   }
 
   const handleNewPerson = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNewPhone = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  const handleNewSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  const ShowPersons = ({personToShow}) => {
+    return (
+      <div>
+        {personToShow.map (person => 
+          <p key={person.name}> 
+          {person.name} 
+          <br /> 
+          {person.phone}</p>
+        )}
+      </div>
+      
+    )
+  }
+
+
+  const filter = persons.filter(person => {
+  return (
+    person.name
+      .toLowerCase()
+      .includes(newSearch.toLowerCase()) || 
+    person.phone
+      .toLowerCase()
+      .includes(newSearch.toLowerCase())
+  );
+});
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input 
+          Name: <input 
+          placeholder='Insert Name'
           value={newName}
           onChange={handleNewPerson}
+          />
+        </div>
+        <div>
+          Phone: <input 
+          placeholder='Insert phone number'
+          value={newPhone}
+          onChange={handleNewPhone}
           />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map (person => 
-        <p key={person.name}> {person.name}</p>
-      )}
+      <h2>Contact List</h2>
+      Search: <input  
+        type='search'
+        placeholder='Search people'
+        value={newSearch}
+        onChange={handleNewSearch}
+      />
+      <ShowPersons personToShow={filter}/>
     </div>
   )
 }
